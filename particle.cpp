646d309bb17fee3e13eb3e329eb9981416e1114d
HeroAdisorn <adisornbkk@gmail.com>
2024-06-07 19:42:16 +0200

    split code to .h and .cpp properly

--- particle.cpp
+++ particle.cpp
@@ -1,1 +1,86 @@
-#include "particle.h"
+#include "Particle.h"
+
+Particle::Particle(const double &mass, const double &charge, const Vector3d &position)
+{
+    m = mass;
+    q = charge;
+    x = position;
+}
+
+void Particle::ResetForce()
+{
+    f.setZero();
+}
+
+void Particle::SetForce(const Vector3d &f_new)
+{
+    f = f_new;
+}
+
+void Particle::AddForce(const Vector3d &df)
+{
+    f += df;
+}
+
+void Particle::ResetVelocity()
+{
+    v.setZero();
+}
+
+void Particle::SetVelocity(const Vector3d &v_new)
+{
+    v = v_new;
+}
+
+void Particle::AddVelocity(const Vector3d &dv)
+{
+    v += dv;
+}
+
+void Particle::SetPostion(const Vector3d &x_new)
+{
+    x = x_new;
+}
+
+void Particle::AddPostion(const Vector3d &dx)
+{
+    x += dx;
+}
+
+void Particle::ResetAll()
+{
+    ResetForce();
+    ResetVelocity();
+}
+
+void Particle::UpdatePosition(const double &dt)
+{
+    // 1st step for velocity Verlet
+    AddPostion(v * dt + 0.5 * f / m * dt * dt);
+    f_prev = f;
+    ResetForce();
+}
+
+void Particle::UpdateVelocity(const double &dt)
+{
+    // 2nd step for velocity Verlet
+    AddPostion(0.5 * (f_prev + f) * dt);
+    ResetForce(); // reset force for the next integration step
+}
+
+void Atom::PPForce(Particle &obj)
+{
+    Vector3d dr = obj.x - x;        // relative postion
+    double r = dr.norm();           // relative distance
+    Vector3d df = Vector3d::Zero(); // interaction force
+    // if other object is also atom type
+    if (dynamic_cast<const Atom *>(&obj))
+    {
+        // Add Lennard-Jones force
+        df += 12 * eps * ((pow(r0, 12) / pow(r, 13)) - (pow(r0, 6) / pow(r, 7))) * dr;
+    }
+    // Add Coulomb force
+    df += q * obj.q / (r * r) * dr;
+    AddForce(df);
+    obj.AddForce(-df);
+}
--- particle.h
+++ particle.h
@@ -1,6 +1,8 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include <vector>
+
 #include "Eigen/Dense"
 using Eigen::Vector3d;
 
@@ -15,75 +17,25 @@ public:
     Vector3d x;      // position
 
     // Constructor
-    Particle(const double &mass, const double &charge, const Vector3d &position)
-    {
-        m = mass;
-        q = charge;
-        x = position;
-    }
+    Particle(const double &mass, const double &charge, const Vector3d &position);
 
-    void ResetForce()
-    {
-        f.setZero();
-    }
+    void ResetForce();
+    void SetForce(const Vector3d &f_new);
+    void AddForce(const Vector3d &df);
 
-    void SetForce(const Vector3d &f_new)
-    {
-        f = f_new;
-    }
+    void ResetVelocity();
+    void SetVelocity(const Vector3d &v_new);
+    void AddVelocity(const Vector3d &dv);
 
-    void AddForce(const Vector3d &df)
-    {
-        f += df;
-    }
+    void SetPostion(const Vector3d &x_new);
+    void AddPostion(const Vector3d &dx);
 
-    void ResetVelocity()
-    {
-        v.setZero();
-    }
+    void ResetAll();
 
-    void SetVelocity(const Vector3d &v_new)
-    {
-        v = v_new;
-    }
+    void UpdatePosition(const double &dt);
+    void UpdateVelocity(const double &dt);
 
-    void AddVelocity(const Vector3d &dv)
-    {
-        v += dv;
-    }
-
-    void SetPostion(const Vector3d &x_new)
-    {
-        x = x_new;
-    }
-
-    void AddPostion(const Vector3d &dx)
-    {
-        x += dx;
-    }
-
-    void ResetAll()
-    {
-        ResetForce();
-        ResetVelocity();
-    }
-
-    void UpdatePosition(const double &dt)
-    {
-        // 1st step for velocity Verlet
-        AddPostion(v * dt + 0.5 * f / m * dt * dt);
-        f_prev = f;
-        ResetForce();
-    }
-
-    void UpdateVelocity(const double &dt)
-    {
-        // 2nd step for velocity Verlet
-        AddPostion(0.5 * (f_prev + f) * dt);
-        ResetForce(); // reset force for the next integration step
-    }
-
-    // virtual method for particle-particle interaction
+    // virtual method declaration for particle-particle interaction
     virtual void PPForce(Particle &obj) = 0;
 };
 
@@ -92,22 +44,8 @@ class Atom : public Particle
 public:
     double r0;  // LJ force bond length
     double eps; // LJ force bond potential energy
-    void PPForce(Particle &obj)
-    {
-        Vector3d dr = obj.x - x;        // relative postion
-        double r = dr.norm();           // relative distance
-        Vector3d df = Vector3d::Zero(); // interaction force
-        // if other object is also atom type
-        if (dynamic_cast<const Atom *>(&obj))
-        {
-            // Add Lennard-Jones force
-            df += 12 * eps * ((pow(r0, 12) / pow(r, 13)) - (pow(r0, 6) / pow(r, 7))) * dr;
-        }
-        // Add Coulomb force
-        df += q * obj.q / (r * r) * dr;
-        AddForce(df);
-        obj.AddForce(-df);
-    }
+
+    void PPForce(Particle &obj) override;
 };
 
 #endif /* end of include guard: PARTICLE_H */
