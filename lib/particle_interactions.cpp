1af53ae75566b91e0687fbf45b3531b32d6eda7e
Adisorn Panasawatwong <64987073+AnAdisorn@users.noreply.github.com>
2024-07-16 17:51:04 +0200

    Update particle_interactions.cpp
    Change pointer to reference

--- lib/particle_interactions.cpp
+++ lib/particle_interactions.cpp
@@ -20,11 +20,11 @@ Vector3d calculateLJForce(const std::string type1, const std::string type2, cons
     r21.normalized();
 
     std::pair<std::string, std::string> pair = createPair(type1, type2);
-    PairParameters *pair_params = &PairParametersMap[pair];
+    PairParameters &pair_params = PairParametersMap[pair];
 
-    double r_ratio = pair_params->kR0 / r21_norm;
+    double r_ratio = pair_params.kR0 / r21_norm;
 
-    return 12 * pair_params->kEps / r21_norm * (pow(r_ratio, 12) - pow(r_ratio, 6)) * r21;
+    return 12 * pair_params.kEps / r21_norm * (pow(r_ratio, 12) - pow(r_ratio, 6)) * r21;
 }
 
 std::array<Vector3d, 2> movePositionLJ(const std::string type1, const std::string type2, const Vector3d &r1, const Vector3d &r2, double dt)
