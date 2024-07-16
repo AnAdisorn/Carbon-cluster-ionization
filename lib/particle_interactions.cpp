df046b9e412985fc9a0ce63062b66fe33f3a4144
Adisorn Panasawatwong <64987073+AnAdisorn@users.noreply.github.com>
2024-07-16 17:54:39 +0200

    Update particle_interactions.cpp

--- lib/particle_interactions.cpp
+++ lib/particle_interactions.cpp
@@ -6,11 +6,11 @@ double calculateLJPotential(const std::string type1, const std::string type2, co
     double r21_norm = r21.norm();
 
     std::pair<std::string, std::string> pair = createPair(type1, type2);
-    PairParameters *pair_params = &PairParametersMap[pair];
+    PairParameters &pair_params = PairParametersMap[pair];
 
-    double r_ratio = pair_params->kR0 / r21_norm;
+    double r_ratio = pair_params.kR0 / r21_norm;
 
-    return pair_params->kEps * (pow(r_ratio, 12) - 2 * pow(r_ratio, 6));
+    return pair_params.kEps * (pow(r_ratio, 12) - 2 * pow(r_ratio, 6));
 }
 
 Vector3d calculateLJForce(const std::string type1, const std::string type2, const Vector3d &r1, const Vector3d &r2)
