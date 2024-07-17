385a25d6e191b145e3290baa5f430be7d22b7946
HeroAdisorn <adisornbkk@gmail.com>
2024-07-17 13:37:09 +0200

    add random position when initialise cluster

--- src/initialise_cluster.cpp
+++ src/initialise_cluster.cpp
@@ -1,18 +1,29 @@
 #include "particle_container.h"
 #include "particle_interactions.h"
 #include "parameters.h"
+
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 #include <vector>
 #include <string>
+
+inline Vector3d randomUnitVector()
+{
+    Vector3d vec = {static_cast<double>(rand()) / RAND_MAX, static_cast<double>(rand()) / RAND_MAX, static_cast<double>(rand()) / RAND_MAX};
+    return vec.normalized();
+}
 
 int main(int argc, char *argv[])
 {
     // Check if given correct number of arguments
     if (!(argc - 1 == 5 || argc - 1 == 6))
     {
-        printf("Requires 5 or 6 inpus but %i were given\n", argc);
+        printf("Requires 5 or 6 inpus but %i were given\n", argc - 1);
         return -1;
     }
+    // Seed the random number generator (optional for better randomness)
+    srand(0);
 
     std::array<int, 3> cubic_sizes; // Size of cubic sides
     double d = 2.91;                // Spacing (default or from argument)
@@ -36,12 +47,17 @@ int main(int argc, char *argv[])
     steps = std::stoi(argv[argc - 2]);
     dt = std::stod(argv[argc - 1]);
 
-    // Open files for writing output
+    // Open files for writing outputs
+    auto cwd = std::filesystem::current_path(); // current working directory
+    std::filesystem::create_directories(cwd / "equilibration/positions");
+    auto positions_dir = cwd / "equilibration/postions";
+    std::ofstream pot_file(cwd / "equilibration/potential.pot"); // file collecting potential at each equilibration step
 
     // Create container for particles
     ParticleContainer container;
     size_t c = 0;
 
+    // Initialise particle in grid
     for (int i = 0; i < cubic_sizes[0]; ++i)
     {
         for (int j = 0; j < cubic_sizes[1]; ++j)
@@ -49,6 +65,7 @@ int main(int argc, char *argv[])
             for (int k = 0; k < cubic_sizes[2]; ++k)
             {
                 Vector3d pos = {i * d, j * d, k * d};
+                pos += d * randomUnitVector() * 0.5;  // add randomness in postion by 50%
                 Vector3d vel = Vector3d::Zero();
                 container.addParticle("carbon_" + std::to_string(c), "C0", pos, vel);
                 c++;
@@ -61,15 +78,18 @@ int main(int argc, char *argv[])
     size_t n = container.size();
     for (int step = 0; step < steps; step++)
     {
+        std::ofstream pos_file(cwd / ("equilibration/positions/" + std::to_string(step) + ".pos"));
         double total_potential_energy = 0; // Collect Lennard-Jones potential energy before equilibrate step
         for (size_t i = 0; i < n; i++)
         {
+            std::string type_i = container.getType(i);
+            Vector3d r_i = container.getPosition(i);
+            // Write position to postions file
+            pos_file << r_i[0] << " " << r_i[1] << " " << r_i[2] << "\n";
+
             for (size_t j = i + 1; j < n; j++)
             {
-                // Get types and postion of paired particles to be adjust
-                std::string type_i = container.getType(i);
                 std::string type_j = container.getType(j);
-                Vector3d r_i = container.getPosition(i);
                 Vector3d r_j = container.getPosition(j);
                 // Calculate the position adjustment according to Lennard-Jones potential
                 auto move_pos = movePositionLJ(type_i, type_j, r_i, r_j, dt);
@@ -81,8 +101,14 @@ int main(int argc, char *argv[])
         }
         // Apply the postion change to all particles
         container.applyPositionChangeAll();
-        printf("Mean potential energy = %f\n", total_potential_energy / n);
+        double mean_potential_energy = total_potential_energy / n;
+        // Write to output file
+        pot_file
+            << mean_potential_energy << "\n";
+        printf("Mean potential energy = %f\n", mean_potential_energy);
+        // Close positions file
+        pos_file.close();
     }
-
+    pot_file.close();
     return 0;
 }
